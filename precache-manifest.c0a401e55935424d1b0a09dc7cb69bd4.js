a7a385ae406cc7995bdc7ddda80ea20659890c2a
LwandoG <lwandogqobho@gmail.com>
2020-07-02 15:32:33 +0200

    Updates

---
+++ precache-manifest.c0a401e55935424d1b0a09dc7cb69bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8a3243af82caa54c084745327863f6",
+    "url": "/lwando/index.html"
+  },
+  {
+    "revision": "b44cc5e7eda22a935069",
+    "url": "/lwando/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "a57d99cdb2e80579e9a7",
+    "url": "/lwando/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "b44cc5e7eda22a935069",
+    "url": "/lwando/static/js/2.f7771b9b.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/lwando/static/js/2.f7771b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57d99cdb2e80579e9a7",
+    "url": "/lwando/static/js/main.b0806fa6.chunk.js"
+  },
+  {
+    "revision": "3c234129282ad3c6064d",
+    "url": "/lwando/static/js/runtime-main.4c9a0b2f.js"
+  }
+]);
