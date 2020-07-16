f0503f7da53d5e7646c8ed2067f5ea44e1fcdda0
LwandoG <lwandogqobho@gmail.com>
2020-07-16 09:16:32 +0200

    Updates

---
+++ precache-manifest.ddc30e21e0b3b26524a9708ffd20e603.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bdff712fd828bfcc5d96669083f07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52d528fe54485a6ec3e7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c2a355f53cb8798c67e",
+    "url": "/static/css/main.6b015033.chunk.css"
+  },
+  {
+    "revision": "52d528fe54485a6ec3e7",
+    "url": "/static/js/2.251cf90a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.251cf90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2a355f53cb8798c67e",
+    "url": "/static/js/main.b74cd346.chunk.js"
+  },
+  {
+    "revision": "15fd58eafa92c84010d6",
+    "url": "/static/js/runtime-main.fca93877.js"
+  },
+  {
+    "revision": "ac1839b05d92a3c9818f9063ad10098e",
+    "url": "/static/media/45-degree-fabric-light.ac1839b0.png"
+  },
+  {
+    "revision": "35e46f85de3c4b3859850a322f00eca2",
+    "url": "/static/media/black-laptop.35e46f85.jpg"
+  },
+  {
+    "revision": "7d91755166fd557af35e3dc8a36dad08",
+    "url": "/static/media/dark-paths.7d917551.png"
+  },
+  {
+    "revision": "62e8f28f4d747b38a5fbd7abd827b7e5",
+    "url": "/static/media/githubfinder.62e8f28f.png"
+  },
+  {
+    "revision": "d12c46fc25f6f8d2a5cc6f75b72058de",
+    "url": "/static/media/lwando.d12c46fc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lwando/precache-manifest.a5a07397fa1e935f4b61d7e623d7dd3a.js"
+  "/precache-manifest.ddc30e21e0b3b26524a9708ffd20e603.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lwando/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
